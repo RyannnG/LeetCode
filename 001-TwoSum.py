7d4971ca936341992d0891c1d3ab6157e69bde40
RyannnG <gy.shouwang@gmail.com>
2016-10-28 13:58:49 +0800

    rename TwoSum to 001-TwoSum

---
+++ 001-TwoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = dict()
+        for i in range(len(nums)):
+            if target - nums[i] in d and i != d[target - nums[i]]:
+                return [i, d[target - nums[i]]]
+            d[nums[i]] = i
+        return None
