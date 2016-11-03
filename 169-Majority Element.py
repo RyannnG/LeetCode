951714df3e0e26417a2c1ea7573199ae7c53ad7d
RyannnG <gy.shouwang@gmail.com>
2016-11-03 09:16:58 +0800

    add Moore voting algorithm

--- 169-Majority Element.py
+++ 169-Majority Element.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+# from collections import defaultdict
 
 class Solution(object):
     def majorityElement(self, nums):
@@ -6,9 +6,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        d = defaultdict()
-        for i in nums:
-            d.setdefault(i, 0)
-            d[i] += 1
+        # time and space O(n)
+        # d = defaultdict()
+        # for i in nums:
+        #     d.setdefault(i, 0)
+        #     d[i] += 1
 
-        return max(d.iteritems(), key=operator.itemgetter(1))[0]
+        # return max(d.iteritems(), key=operator.itemgetter(1))[0]
+        
+        # Moore Voting
+        # time O(n), space O(1)
+        idx = 0
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[idx] == nums[i]:
+                count += 1
+            else:
+                count -= 1
+            
+            if count == 0:
+                idx = i
+                count = 1
+                
+        return nums[idx]
